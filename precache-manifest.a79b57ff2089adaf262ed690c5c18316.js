796ddb7ba874669d13e4b4d42acaac28391f2576
Shounak Amladi <shounakamladi@cmail.carleton.ca>
2020-05-02 08:44:29 -0400

    Updates

---
+++ precache-manifest.a79b57ff2089adaf262ed690c5c18316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73294db160dc245172a0c6130997d2b3",
+    "url": "/valorant-map-viewer/index.html"
+  },
+  {
+    "revision": "61e79087b8c9419c3405",
+    "url": "/valorant-map-viewer/static/css/main.8dd7cdb2.chunk.css"
+  },
+  {
+    "revision": "714edb5aa29911737277",
+    "url": "/valorant-map-viewer/static/js/2.ce262347.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/valorant-map-viewer/static/js/2.ce262347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e79087b8c9419c3405",
+    "url": "/valorant-map-viewer/static/js/main.f23ba343.chunk.js"
+  },
+  {
+    "revision": "cd099bdfbb6749ea5d2a",
+    "url": "/valorant-map-viewer/static/js/runtime-main.53fe6534.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/valorant-map-viewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
